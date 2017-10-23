f87b7dca592793787b720819932bf99b010b01b1
Łukasz Kettler <lukaszx.kettler@intel.com>
2017-10-23 16:51:17 +0200

    Adding process: setting adding condition in InitialPoo class

--- src/solitaire5/InitialPoo.java
+++ src/solitaire5/InitialPoo.java
@@ -7,21 +7,28 @@ package solitaire5;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
+import java.util.function.BiConsumer;
 import javafx.event.Event;
 
 /**
  *
  * @author Lukas
  */
-public class InitialPoo extends Base {
+public class InitialPoo extends Base implements BiConsumer<List<Card>, InitialPoo>{
     private List<Card> cardsOnPoo;
     protected double oyShift;
+    private BiConsumer<List<Card>, InitialPoo> addCond;
     
     public InitialPoo() {
         super("file:src\\res\\dickbutt.png");
         cardsOnPoo  = new ArrayList<>();
         oyShift = 0;
+        addCond = (cards, oldPoo) -> {
+            oldPoo.removeFromPoo(cards);
+            this.addToPoo(cards); // TO DO - REVERSE ORDER?
+        };
     }
     
     public void addToPoo(Card card){
@@ -40,6 +47,15 @@ public class InitialPoo extends Base {
     
     public void removeFromPoo(List<Card> cards){
         cardsOnPoo.removeAll(Arrays.asList(cards));
+    }
+    
+    protected void setAddCond(BiConsumer<List<Card>, InitialPoo> cond){
+        this.addCond = cond;
+    }
+
+    @Override
+    public void accept(List<Card> cards, InitialPoo oldPoo) {
+        addCond.accept(cards, this);
     }
     
 }
